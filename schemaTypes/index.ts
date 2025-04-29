9a2358be7f81141bddb1ebd544507f2c305b5a1f
Christian Colson <christiancolson91@gmail.com>
2025-04-28 21:41:10 -0500

    my work section - sanity data schema and adding data

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import {devExperience} from './devExperience'
+import {project} from './project'
 
-export const schemaTypes = [devExperience]
+export const schemaTypes = [devExperience,project]
 
 
